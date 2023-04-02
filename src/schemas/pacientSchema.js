72a8763f3d91d6b97eb8a3cb09d2a2ceaf273540
Thiago <trabalho.bandeira@gmail.com>
2023-04-02 19:38:05 -0300

    fix: pacient schema typo

--- src/schemas/pacientSchema.js
+++ src/schemas/pacientSchema.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const pacientSchema = Joi.object({
-  name: Joi.string().min(3).required,
+  name: Joi.string().min(3).required(),
   email: Joi.string(6).email().required(), 
   password: Joi.string().min(5).required(),
   dateOfBirth: Joi.date().raw().required(),
