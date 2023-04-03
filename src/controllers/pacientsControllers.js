e2bd22022bf8eeba3f786d874e1d00dcd2daa86f
Thiago <trabalho.bandeira@gmail.com>
2023-04-02 23:14:59 -0300

    feat: pacientsControllers

---
+++ src/controllers/pacientsControllers.js
@@ -0,0 +1,14 @@
+
+async function create(req, res, next){
+  const{name, email, password, dateOfBirth} = res.locals.data
+  try {
+    await pacientsServices.create({name, email, password, dateOfBirth})
+    res.sendStatus(201)   
+  } catch (error) {
+    next(error)
+  }
+}
+
+export default {
+  create
+}
--- src/routes/pacients.routes.js
+++ src/routes/pacients.routes.js
@@ -1,9 +1,12 @@
 //resposability: center all HTTP verbs for a resource in a single router func.
 import { Router } from "express";
+import { schemaValidator } from "../middlewares/schemaValidator.js";
+import { pacientSchema } from "../schemas/pacientSchema.js";
+import pacientsControllers from "../controllers/pacientsControllers.js";
 
 const pacientsRouter = Router();
 
-pacientsRouter.post("/signup");
+pacientsRouter.post("/signup",schemaValidator(pacientSchema), pacientsControllers.create);
 pacientsRouter.get("/");
 
 export default pacientsRouter;
