5c94a8cd0fabc8e2633b491f0adb8d2fe0a562db
Thiago <trabalho.bandeira@gmail.com>
2023-04-02 22:58:20 -0300

    fix: doctors and pacient schemas

--- src/schemas/pacientSchema.js
+++ src/schemas/pacientSchema.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const pacientSchema = Joi.object({
   name: Joi.string().min(3).required(),
-  email: Joi.string(6).email().required(), 
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'io'] } }).required(), 
   password: Joi.string().min(5).required(),
   dateOfBirth: Joi.date().raw().required(),
 })
